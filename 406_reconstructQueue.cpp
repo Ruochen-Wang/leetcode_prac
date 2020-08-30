a7b36aaf4305dbe5e3f8b7160c2bad65fae79241
Ruochen-Wang <rwang113@terpmail.umd.edu>
2020-08-29 22:26:07 -0400

    tried to solve problem 406

---
+++ 406_reconstructQueue.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class leetcode_solution{
+public:
+    vector<vector<int>> reconstructQueue(vector<vector<int>>& people) {
+        for(vector<int> pair: people){
+            pair
+        }
+    }
+}
+
+int main(){
+    return 0;
+}
---
+++ 877_stoneGame.py
@@ -0,0 +1,9 @@
+from typing import List
+
+def stoneGame(piles: List[int]) -> bool:
+    return True
+
+
+if __name__ == "__main__":
+    piles = [5, 3, 4, 5]
+    print(stoneGame(piles))
